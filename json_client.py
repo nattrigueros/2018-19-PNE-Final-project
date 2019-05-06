b3b7aa458a80fc4a569807ba3f03587a3f347beb
nattrigueros <nattrigueros00@gmail.com>
2019-05-06 19:54:29 +0200

    This is the Json Client for my Final Project :)

---
+++ json_client.py
@@ -0,0 +1,91 @@
+import http.client
+import json
+
+PORT = 8000
+SERVER = 'localhost'
+
+print("\nConnecting to server: {}:{}\n".format(SERVER, PORT))
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER, PORT)
+
+# SOME REQUESTS FOR OUR SERVER OF EVERY ENDPOINT AND ALSO OF ENDPOINTS WITH VARIATION IN THE ORDER OF PARAMETERS
+
+conn.request("GET", "/listSpecies?limit=121&json=1")
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
+
+conn.request("GET", "/listSpecies?limit=&json=1")
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
+
+conn.request("GET", "/listSpecies?&json=1")
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
+
+conn.request("GET", "/karyotype?specie=mouse&json=1")
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
+
+conn.request("GET", "/chromosomeLength?specie=mouse&chromo=1&json=1")
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
+
+conn.request("GET", "/geneSeq?gene=FRAT1&json=1")
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
+
+conn.request("GET", "/geneInfo?gene=FRAT1&json=1")
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
+conn.request("GET", "/geneCal?gene=FRAT1&json=1")
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
+
+conn.request("GET", "/geneList?chromo=1&start=0&end=30000&json=1")
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
+
+
+# CHANGE THE ORDER OF THE PARAMETERS IN THE URL
+
+conn.request("GET", "/chromosomeLength?json=1&specie=mouse&chromo=1")
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
+
+conn.request("GET", "/geneList?chromo=1&start=0&json=1&end=30000")
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
